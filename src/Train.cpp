e12122080c8470b421daca4bbc89b1c5d8958944
Anthony Todesco <adtodesco@gmail.com>
2016-09-10 09:48:15 -0600

    writing optionParser

--- src/Train.cpp
+++ src/Train.cpp
@@ -1,22 +1,5 @@
 #include "Train.h"
 
-enum  optionIndex { UNKNOWN, HELP, PLUS };
-const option::Descriptor usage[] =
-{
- {UNKNOWN, 0, "", "",option::Arg::None, "USAGE: trainer <trainer_file> [options]\n\n"
-                                        "Options:" },
- {PLUS, 0,"i","input-nodes",option::Arg::int, "  -i, --input-nodes \tNumber of input nodes." },
- {PLUS, 0,"h","hidden-nodes",option::Arg::int, "  -h, --hidden-nodes \tNumber of hidden nodes." },
- {PLUS, 0,"o","output-nodes",option::Arg::int, "  -o, --output-nodes \tNumber of output nodes." },
- {PLUS, 0,"l","hidden-layers",option::Arg::int, "  -h, --hidden-layers \tNumber of hidden layers." },
- {PLUS, 0,"w","weight-file",option::Arg::string, "  -w, --weight-file \tName of weight file." },
- {HELP, 0,"", "help",option::Arg::None, "  --help  \tPrint usage and exit." },
- {UNKNOWN, 0, "", "",option::Arg::None, "\nExamples:\n"
-                               "  trainer trainer_file.tra -i 10 -h 5 -o 2 -h 1\n"
-                               "  trainer \n" },
- {0,0,0,0,0,0}
-};
-
 // Returns the path to the default weights directory
 std::string getWeightsDir(char* argvZero) {
   std::string executablePath = realpath(argvZero, NULL);
@@ -24,32 +7,30 @@ std::string getWeightsDir(char* argvZero) {
   return executablePath.substr(0, index) + "/files/weights";
 }
 
+// Parse options
+std::vector<std::string> parseOptions(int argc, char* argv[]) {
+  std::string trainingFile = NULL;
+  std::string hiddenNodes = "0";
+  std::string hiddenLayers = "0"; 
+
+  std::vector<std::string> returnString;
+  
+  for (int i = 0; i < argc; i++) {
+    std::cout << "argv[" << i << "] = " << argv[i] << std::endl;
+    switch(argv[i].to_s) {
+      case "-h", "--hidden-nodes":
+        std::cout << "Setting hidden nodes to " << argv[i] << std::endl;
+      case "-l", "--hidden-layers":
+        std::cout << "Setting hidden layers to " << argv[i] << std::endl;
+    }
+  }
+  return returnString;
+}
+
 int main(int argc, char* argv[])
 {
 
- argc-=(argc>0); argv+=(argc>0); // skip program name argv[0] if present
-  option::Stats  stats(usage, argc, argv);
-  std::vector<option::Option> options(stats.options_max);
-  std::vector<option::Option> buffer(stats.buffer_max);
-  option::Parser parse(usage, argc, argv, &options[0], &buffer[0]);
-
-  if (parse.error())
-    return 1;
-
-  if (options[HELP] || argc == 0) {
-    option::printUsage(std::cout, usage);
-    return 0;
-  }
-
-  std::cout << "--plus count: " <<
-      options[PLUS].count() << "\n";
-
-  for (option::Option* opt = options[UNKNOWN]; opt; opt = opt->next())
-    std::cout << "Unknown option: " << std::string(opt->name,opt->namelen) << "\n";
-
-  for (int i = 0; i < parse.nonOptionsCount(); ++i)
-    std::cout << "Non-option #" << i << ": " << parse.nonOption(i) << "\n";
-
+  parseOptions(argc, argv);
 
   Network myNeuralNet = Network(2, 5, 2, 1);
   //Network myNeuralNet = Network(10, 5, 7, 1);
