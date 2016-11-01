ab3fb34009cadb0ed303f2786a5b00eb5b71a784
Anthony Todesco <adtodesco@gmail.com>
2016-10-31 22:40:56 -0600

    working on test - found bug

--- tests/sine.py
+++ tests/sine.py
@@ -1,12 +1,20 @@
 import random
 import math
 
-def writeTrainingFile(function):
+def writeTempFiles(function):
+  trainDir = "./files/train/"
+  testDir = "./files/tests/"
+
+  trainTarget = open(trainDir + function + '.csv', 'w')
+  testTarget = open(testDir + function + '.csv', 'w')
+
   for i in range(20):
     if function == 'sine':
        temp = random.uniform(0, 9)
-       target = open(filename, 'w')
-       print("%s %s" % (temp, round(math.sin(temp))))
+       trainTarget.write("%s,%s,\n" % (temp, int(round(math.sin(temp) + 1))))
+       temp = random.uniform(0, 9)
+       testTarget.write("%s,%s,\n" % (temp, int(round(math.sin(temp) + 1))))
 
 if __name__ == '__main__':
-  writeTrainingFile('sine')
+  writeTempFiles('sine')
+  
